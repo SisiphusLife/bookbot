576a8e4fddd44189decb5c1052d71022de219568
thimorichter91 <thimorichter91@gmail.com>
2025-01-28 20:48:34 +0100

    C:

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def count_characters(text):
     char_count = {} 
     for char in text:
         lowercase_char = char.lower()
-       if lowercase_char in char_count:
+        if lowercase_char in char_count:
             char_count[lowercase_char] = char_count[lowercase_char] + 1
         else:
             char_count[lowercase_char] = 1
@@ -16,7 +16,9 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         word_count = count_words(file_contents)
-        print (word_count)
+        char_count = count_characters(file_contents)
+        print(word_count)
+        print(char_count)
 
 if __name__ == "__main__":
     main()
